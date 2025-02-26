f78740379c83be356ea3907a63e6715e65ddf63d
hungbui0905 <81497318+hungbui0905@users.noreply.github.com>
2025-02-26 15:23:40 +0700

    init project Tri Duc Clinic

---
+++ javascripts/home.js
@@ -0,0 +1,64 @@
+function toggleMenu() {
+    var menuIcon = document.getElementById("menuIcon");
+    menuIcon.classList.toggle("active");
+
+    checkWidth();
+}
+
+function toggleSearch() {
+    var inputSearch = document.getElementById("searchIcon");
+    inputSearch.classList.toggle("active");
+
+    checkWidth();
+}
+
+function checkWidth() {
+    let picContent = document.getElementById("picContent");
+    let menuIconItem = document.getElementById("menuIconItem");
+    let searchIconInput = document.getElementById("searchIconInput");
+
+    let menuIconStyle = window.getComputedStyle(menuIconItem);
+    let searchIconStyle = window.getComputedStyle(searchIconInput);
+    if (window.innerWidth > 987) {
+        picContent.style.marginTop = "0px";
+    } else if(menuIconStyle.display !== "none"){
+        console.log("Menu")
+        picContent.style.marginTop = menuIconItem.offsetHeight + 20 + "px";
+    } else if (searchIconStyle.display === "block") {
+        console.log("Search");
+        picContent.style.marginTop = searchIconInput.offsetHeight + 20 + "px";
+    } else {
+        picContent.style.marginTop = "0px";
+    }
+
+}
+
+window.addEventListener("resize", checkWidth);
+
+
+function toggleSearchInput() {
+    let searchInput = document.getElementById("searchInput");
+    searchInput.style.border = "none";
+}
+
+
+
+function moveClinicInfo() {
+    let mediaQuery = window.innerWidth;
+    let parentDiv = document.getElementById("menuSearch");
+    let childDiv = document.getElementById("clinicInfo");
+
+    if (mediaQuery <= 987 && mediaQuery >= 540) {
+        let searchPhone = document.getElementById("searchPhone");
+        parentDiv.insertBefore(childDiv, searchPhone);
+    } else {
+        let menuBar = document.getElementById("menuBar");
+        document.body.insertBefore(childDiv, menuBar);
+    }
+}
+
+moveClinicInfo();
+
+window.addEventListener("resize", moveClinicInfo);
+window.addEventListener("resize", moveClinicInfo);
+window.addEventListener("DOMContentLoaded", moveClinicInfo);

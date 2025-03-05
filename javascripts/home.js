cb21f462a4439452009e2922fe6b11f92a13d8e9
hungbui0905 <81497318+hungbui0905@users.noreply.github.com>
2025-03-05 18:07:21 +0700

    Change some texts

--- javascripts/home.js
+++ javascripts/home.js
@@ -1,21 +1,41 @@
 function toggleMenu() {
     var menuIcon = document.getElementById("menuIcon");
     menuIcon.classList.toggle("active");
+
+    checkWidth();
 }
 
 function toggleSearch() {
     var inputSearch = document.getElementById("searchIcon");
     inputSearch.classList.toggle("active");
 
+    checkWidth();
 }
+
+function checkWidth() {
+    let picContent = document.getElementById("picContent");
+    let menuIconItem = document.getElementById("menuIconItem");
+    let searchIconInput = document.getElementById("searchIconInput");
+    let menuIconStyle = window.getComputedStyle(menuIconItem);
+    let searchIconStyle = window.getComputedStyle(searchIconInput);
+    if (window.innerWidth > 987) {
+        picContent.style.marginTop = "0px";
+    } else if(menuIconStyle.display === "block"){
+        picContent.style.marginTop = menuIconItem.offsetHeight + "px";
+    } else if (searchIconStyle.display === "block") {
+        picContent.style.marginTop = searchIconInput.offsetHeight+ "px";
+    } else {
+        picContent.style.marginTop = "0px";
+    }
+}
+
+window.addEventListener("resize", checkWidth);
+
 
 function toggleSearchInput() {
     let searchInput = document.getElementById("searchInput");
     searchInput.style.border = "none";
 }
-
-
-
 
 function moveClinicInfo() {
     let mediaQuery = window.innerWidth;

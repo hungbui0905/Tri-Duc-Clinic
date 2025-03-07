4b3d546316d67a71c3bacf7208df96bb98d21914
hungbui0905 <81497318+hungbui0905@users.noreply.github.com>
2025-03-07 15:20:16 +0700

    Upload changes

--- javascripts/contact.js
+++ javascripts/contact.js
@@ -1,11 +1,15 @@
 function h3alignment() {
-    let namePhone= document.getElementById("namePhone");
-    let phoneInput= document.getElementById("phoneInput");
-    let npWidth= namePhone.offsetWidth;
-    let inputWidth= phoneInput.offsetWidth;
-    let paddingNum= (npWidth-(inputWidth*2))/2;
+    let namePhone = document.getElementById("namePhone");
+    let phoneInput = document.getElementById("phoneInput");
+    let npWidth = namePhone.offsetWidth;
+    let inputWidth = phoneInput.offsetWidth;
+    let paddingNum = (npWidth - (inputWidth * 2)) / 2;
     let h3Element = document.getElementById("h3Element");
-    h3Element.style.marginRight= paddingNum+"px";
+    h3Element.style.paddingRight = paddingNum + "px";
 }
+
 h3alignment();
-window.addEventListener("resize", h3alignment)
+
+
+window.addEventListener("DOMContentLoaded", h3alignment);
+window.addEventListener("resize", h3alignment);
